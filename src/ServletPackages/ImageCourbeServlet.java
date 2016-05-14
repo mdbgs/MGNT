e4529ab2a9681afae5eaea88c90814f63d276042
MoussaPC <MoussaPC@10.105.58.164>
2016-05-14 11:47:52 +0000

    Correction d'erreur

--- src/ServletPackages/ImageCourbeServlet.java
+++ src/ServletPackages/ImageCourbeServlet.java
@@ -8,16 +8,14 @@ import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//import org.jfree.chart.ChartFactory;
-//import org.jfree.chart.ChartUtilities;
-//import org.jfree.chart.JFreeChart;
-//import org.jfree.chart.plot.PlotOrientation;
-//import org.jfree.data.category.DefaultCategoryDataset;
-//import org.jfree.data.general.PieDataset;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
 
 import BeanPackage.ComputeQueryBean;
 
@@ -42,24 +40,24 @@ public class ImageCourbeServlet extends ConnexionServlet {
 		}
 		 ResultSet  rs = ComputeQueryBean.selectnum("indicateur", connection);
 		 
-//		DefaultCategoryDataset data=new DefaultCategoryDataset();
-//		try {
-//			while(rs.next()){
-////			data.setValue(rs.getInt(2),rs.getString(1),"EPSC");}
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		}
+		DefaultCategoryDataset data=new DefaultCategoryDataset();
+		try {
+			while(rs.next()){
+			data.setValue(rs.getInt(2),rs.getString(1),"EPSC");}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 //		data.setValue(98,"entretien preventif systhématique non contractuel","EPSNC");
 //		data.setValue(12,"entretien préventif conditionnelle","EPC");
 //		data.setValue(65.67,"entretien preventif global","EPG");
 //		data.setValue(100,"entretien curatif","EC");
 //		data.setValue(95,"Autres","Autre");
-//	    JFreeChart graphico=ChartFactory.createBarChart("Diagramme en barre des activités en fonction des indicateurs" ,"Indicateurs","Activités",data,PlotOrientation.VERTICAL,true,true,true);
+	    JFreeChart graphico=ChartFactory.createBarChart("Diagramme en barre des activités en fonction des indicateurs" ,"Indicateurs","Activités",data,PlotOrientation.VERTICAL,true,true,true);
 	    // JFreeChart graphe = ChartFactory.createPieChart("", (PieDataset) data, true, true, false);
 		//JFreeChart graphe = ChartFactory.createPieChart(titre, defaultpiedataset, true, true, false);
 		response.setContentType("image/JPEG");
 		OutputStream sa=response.getOutputStream(); 
-//		ChartUtilities.writeChartAsJPEG(sa,graphico,600,500);
+		ChartUtilities.writeChartAsJPEG(sa,graphico,600,500);
 
 	}
 
